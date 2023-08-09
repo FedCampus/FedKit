f4bcf977bc1e7de23a5d94b9e6867c401f2dd849
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-08-09 23:10:32 +0800

    ChatGPT-generated data structures translation

--- flutter/lib/backend_client.dart
+++ flutter/lib/backend_client.dart
@@ -11,17 +11,101 @@ class BackendClient {
   final String url;
   BackendClient(this.url);
 
-  Future<TFLiteModel> advertisedModel(PostAdvertisedData body) async => dio
-      .post('$url/advertised_model', data: body.toJson())
-      .then((res) => TFLiteModel.fromJson(res.data));
+  Future<TFLiteModel> advertisedModel(PostAdvertisedData body) async {
+    Response response =
+        await dio.post('$url/train/advertised', data: body.toJson());
+    return TFLiteModel.fromJson(response.data);
+  }
+
+  // FIXME: Lacking args. Use Dio.download.
+  Future<ResponseBody> downloadFile(String url) async {
+    Response<ResponseBody> response = await dio.get<ResponseBody>(
+      url,
+      options: Options(responseType: ResponseType.stream),
+    );
+    return response.data!;
+  }
+
+  Future<ServerData> postServer(TFLiteModel model, bool startFresh) async {
+    PostServerData body = PostServerData(model.id, startFresh);
+    Response response =
+        await dio.post('$url/train/server', data: body.toJson());
+    return ServerData.fromJson(response.data);
+  }
+
+  Future<void> fitInsTelemetry(FitInsTelemetryData body) async {
+    await dio.post('$url/telemetry/fit_ins', data: body.toJson());
+  }
+
+  Future<void> evaluateInsTelemetry(EvaluateInsTelemetryData body) async {
+    await dio.post('$url/telemetry/evaluate_ins', data: body.toJson());
+  }
 }
 
 @JsonSerializable()
 class PostAdvertisedData {
-  String data_type;
+  final String data_type;
   PostAdvertisedData(this.data_type);
 
   factory PostAdvertisedData.fromJson(Map<String, dynamic> json) =>
       _$PostAdvertisedDataFromJson(json);
   Map<String, dynamic> toJson() => _$PostAdvertisedDataToJson(this);
+}
+
+// Always change together with Python `train.data.ServerData`.
+@JsonSerializable()
+class ServerData {
+  final String status;
+  final int? session_id;
+  final int? port;
+
+  ServerData(this.status, this.session_id, this.port);
+
+  factory ServerData.fromJson(Map<String, dynamic> json) =>
+      _$ServerDataFromJson(json);
+  Map<String, dynamic> toJson() => _$ServerDataToJson(this);
+}
+
+@JsonSerializable()
+class PostServerData {
+  final int id;
+  final bool start_fresh;
+
+  PostServerData(this.id, this.start_fresh);
+
+  Map<String, dynamic> toJson() => _$PostServerDataToJson(this);
+}
+
+// Always change together with Python `telemetry.models.FitInsTelemetryData`.
+@JsonSerializable()
+class FitInsTelemetryData {
+  final int device_id;
+  final int session_id;
+  final int start;
+  final int end;
+
+  FitInsTelemetryData(this.device_id, this.session_id, this.start, this.end);
+
+  factory FitInsTelemetryData.fromJson(Map<String, dynamic> json) =>
+      _$FitInsTelemetryDataFromJson(json);
+  Map<String, dynamic> toJson() => _$FitInsTelemetryDataToJson(this);
+}
+
+// Always change together with Python `telemetry.models.EvaluateInsTelemetryData`.
+@JsonSerializable()
+class EvaluateInsTelemetryData {
+  final int device_id;
+  final int session_id;
+  final int start;
+  final int end;
+  final double loss;
+  final double accuracy;
+  final int test_size;
+
+  EvaluateInsTelemetryData(this.device_id, this.session_id, this.start,
+      this.end, this.loss, this.accuracy, this.test_size);
+
+  factory EvaluateInsTelemetryData.fromJson(Map<String, dynamic> json) =>
+      _$EvaluateInsTelemetryDataFromJson(json);
+  Map<String, dynamic> toJson() => _$EvaluateInsTelemetryDataToJson(this);
 }
