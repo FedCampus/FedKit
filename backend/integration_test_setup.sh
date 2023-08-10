784a7ad5b534758e71d74b45d576998b63c9b4c1
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-08-10 08:41:10 +0800

    finish backend setup before flutter test

--- backend/integration_test_setup.sh
+++ backend/integration_test_setup.sh
@@ -1,6 +1,5 @@
 pip install -r requirements.txt
-pythob manage.py migrate
+python manage.py migrate
 mkdir static
 touch static/cifar10.tflite
 cat seed.py | python manage.py shell
-python manage.py runserver
