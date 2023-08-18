cf82d762d2312eca118cb6cb9f6ba19dc72b34b8
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-08-18 16:03:07 +0800

    fake ios training in fed_kit_client

--- fed_kit_client/ios/Runner/AppDelegate.swift
+++ fed_kit_client/ios/Runner/AppDelegate.swift
@@ -3,6 +3,7 @@ import UIKit
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+    private var parameters: [FlutterStandardTypedData]?
     override func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -12,16 +13,57 @@ import UIKit
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
-    public func register() {
+    func register() {
         let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
         let channel = FlutterMethodChannel(name: "fed_kit_client_cifar10_ml_client", binaryMessenger: controller.binaryMessenger)
         channel.setMethodCallHandler(handle)
     }
 
-    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
         case "getPlatformVersion": result("iOS " + UIDevice.current.systemVersion)
+        case "evaluate": evaluate(result)
+        case "fit": fit(call, result)
+        case "getParameters": getParameters(result)
+        case "ready": ready(result)
+        case "testSize": result(Int(9)) // TODO: Actual test size.
+        case "trainingSize": result(Int(99)) // TODO: Actual training size.
+        case "updateParameters": updateParameters(call, result)
+        case "initML": initML(call, result)
         default: result(FlutterMethodNotImplemented)
         }
+    }
+
+    func evaluate(_ result: @escaping FlutterResult) {
+        // TODO: Real evaluation.
+        var fakeData = [Float32(0.9), Float32(0.1)]
+        result(FlutterStandardTypedData(float32: Data(buffer: UnsafeBufferPointer(start: fakeData, count: 2))))
+    }
+
+    func fit(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) {
+        // TODO: Real fit.
+        result(nil)
+    }
+
+    func getParameters(_ result: @escaping FlutterResult) {
+        // TODO: Real parameters.
+        result(parameters)
+    }
+
+    func ready(_ result: @escaping FlutterResult) {
+        // TODO: Test.
+        result(true)
+    }
+
+    func updateParameters(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) {
+        let args = call.arguments as! [String: Any]
+        parameters = args["parameters"] as? [FlutterStandardTypedData]
+        // TODO: Real updates.
+        result(nil)
+    }
+
+    func initML(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) {
+        // TODO: Initialize model and load data.
+        result(nil)
     }
 }
