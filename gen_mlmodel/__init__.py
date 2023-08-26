7c303428fd69fd664c8a8d35cccabbbf91da86fb
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-08-26 09:28:36 +0800

    mnist.mlmodel generation

--- gen_mlmodel/__init__.py
+++ gen_mlmodel/__init__.py
@@ -16,15 +16,18 @@ def nn_builder(mlmodel: MLModel) -> NeuralNetworkBuilder:
 
 def try_make_layers_updatable(builder: NeuralNetworkBuilder):
     assert builder.nn_spec is not None
+    updatable_layers: list[str] = []
     for layer in builder.nn_spec.layers:
         name = layer.name
         try:
             builder.make_updatable([name])
+            updatable_layers.append(name)
             print(f"made {name} updatable")
         except KeyboardInterrupt:
             raise KeyboardInterrupt
         except Exception:
             print(f"could not make {name} updatable")
+    return updatable_layers
 
 
 def save_builder(builder: NeuralNetworkBuilder, directory: str):
