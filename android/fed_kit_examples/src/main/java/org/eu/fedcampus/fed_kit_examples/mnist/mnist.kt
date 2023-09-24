4658ef2f06f9520b6586c4d48e1ecce9ec4e45e7
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-09-24 13:26:33 +0800

    fix android mnist training by changing Y_SHAPE

--- android/fed_kit_examples/src/main/java/org/eu/fedcampus/fed_kit_examples/mnist/mnist.kt
+++ android/fed_kit_examples/src/main/java/org/eu/fedcampus/fed_kit_examples/mnist/mnist.kt
@@ -5,7 +5,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.eu.fedcampus.fed_kit_examples.cifar10.Float3DArray
-import org.eu.fedcampus.fed_kit_examples.cifar10.IMAGE_SIZE
 import org.eu.fedcampus.fed_kit_train.FlowerClient
 import org.eu.fedcampus.fed_kit_train.SampleSpec
 import org.eu.fedcampus.fed_kit_train.helpers.classifierAccuracy
@@ -54,15 +53,15 @@ private fun addSample(
     val splits = line.split(",")
     val feature = Array(IMAGE_SIZE) { Array(IMAGE_SIZE) { FloatArray(1) } }
     val label = FloatArray(N_CLASSES)
-    for (i in 0..LENGTH_ENTRY) {
-        feature[i / IMAGE_SITE][i % IMAGE_SITE][0] = splits[i].toFloat() / NORMALIZATION
+    for (i in 0 until LENGTH_ENTRY) {
+        feature[i / IMAGE_SIZE][i % IMAGE_SIZE][0] = splits[i].toFloat() / NORMALIZATION
     }
     label[splits.last().toInt()] = 1f
     flowerClient.addSample(feature, label, isTraining)
 }
 
 private const val TAG = "MNIST Data Loader"
-private const val IMAGE_SITE = 28
+private const val IMAGE_SIZE = 28
 private const val N_CLASSES = 10
-private const val LENGTH_ENTRY = IMAGE_SITE * IMAGE_SITE
+private const val LENGTH_ENTRY = IMAGE_SIZE * IMAGE_SIZE
 private const val NORMALIZATION = 255f
